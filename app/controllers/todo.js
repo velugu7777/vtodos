a0688f392a02118ae0effae5cd873bfe6576fcb0
velugu7777 <velugu.go@gmail.com>
2015-07-03 00:15:15 +0530

    update

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-ktodo: "",
+ktodo:"",
  actions:{
-  newTodo:function(kkk){
-    var k = {title: kkk, isCompleted: false};
-    console.log(k);
+  newTodo:function(post){
+  //  this.sendAction("internalAction");
+  //  var k = {title: kkk, isCompleted: false};
+    //console.log(k);
+  //  newTodo.push("Todos");
+  //  console.log(newTodo);
   }
 }
+});
+App.PostController = Ember.objectController.extend({
+  actions:{
+    newTodo:function(post){
+      console.log(post.get("title"));
+    }
+
+  }
 });
--- app/routes/todo.js
+++ app/routes/todo.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model:function(){
     return [
-      {title: "hi", isCompleted: true},
-      {title: "kkkk", isCompleted: true}
-    ]//this.store.find("todo");
+      {title: "", isCompleted: false},
+      {title: "", isCompleted: false}
+   ];//this.store.find("todo");
   }
 });
