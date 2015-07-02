dd0698513c00ff0b94207df81c6a57834a32313c
velugu7777 <velugu.go@gmail.com>
2015-07-02 11:52:37 +0530

    update

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import('./bower_components/bootstrap/dist/css/bootstrap.min.css');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
