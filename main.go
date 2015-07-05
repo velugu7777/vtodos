4fff9910dd88f38b1053f26f191623c7d797000a
velugu7777 <velugu.go@gmail.com>
2015-07-05 16:08:16 +0530

    updated

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Todo struct {
-	Id     int
-	Name   string
+	id     int
+	name   string
 	IsDone bool
 }
 
@@ -17,43 +17,37 @@ var mytodos []Todo
 
 func init() {
 	t1 := Todo{
-		Id:     1,
-		Name:   "Reading books",
+		id:     1,
+		name:   "Reading books",
 		IsDone: false,
 	}
 	t2 := Todo{
-		Id:     2,
-		Name:   "Playing Cricket",
+		id:     2,
+		name:   "playing cricket",
 		IsDone: false,
 	}
 	mytodos = append(mytodos, t1, t2)
 }
-
 func main() {
-	goji.Get("/todos", todos)
-	goji.Post("/todos/newTodo", newTodo)
-	goji.Get("/todos/:id", getTodo)
-	goji.Put("/todos/:id", putTodo)
-	goji.Delete("/todos/:id", delTodo)
+	goji.Get("/todo", todos)
+	goji.Post("/todo/newTodo", newTodo)
+	goji.Get("/todo:id", getTodo)
+	goji.Put("/todo:id", putTodo)
+	goji.Delete("/todo:id", delTodo)
 	goji.Serve()
 }
-
 func todos(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "%+v", mytodos)
 }
-
 func newTodo(w http.ResponseWriter, req *http.Request) {
 
 }
-
 func getTodo(c web.C, w http.ResponseWriter, req *http.Request) {
 
 }
-
 func putTodo(c web.C, w http.ResponseWriter, req *http.Request) {
 
 }
-
 func delTodo(c web.C, w http.ResponseWriter, req *http.Request) {
 
 }
